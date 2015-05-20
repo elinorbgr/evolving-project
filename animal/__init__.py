27b3b20e490b78c6621558a1872ba52c0c163a60
Victor Berger <victor.berger@m4x.org>
2015-05-20 22:36:16 +0200

    Integrate brains.

--- animal/__init__.py
+++ animal/__init__.py
@@ -1,19 +1,28 @@
+import math
+
 from neural.brain import Brain
 
-class AnimalOutput:
-    def __init__(self, speed, angular_speed, outputs):
-        self.speed = speed
-        self.angular_speed = angular_speed
-        self.outputs = outputs
-
 class Animal:
+
+    theta = 0
+    x = 0
+    y = 0
+
     def __init__(self, genome):
         self.brain = Brain(genome, ["Ea"], ["Mx", "My"])
         self.energy = 50.0
 
-    def update(self, inputs):
+    def update(self, inputs, deltatime, v_scale, angv_scale):
         (hard_out, outputs) = self.brain.compute({ "Ea": self.energy }, inputs)
         speed = hard_out["Mx"]
         angular_speed = hard_out["My"]
-        self.energy -= 0.01 + 0.1 * speed
-        return AnimalOutput(speed, angular_speed, outputs)
+        self.energy -= 0.01 + 0.1 * speed * deltatime
+        self.x += math.cos(self.theta) * speed * deltatime * v_scale
+        self.y += math.sin(self.theta) * speed * deltatime * v_scale
+        self.theta += angular_speed * deltatime * angv_scale
+        return outputs
+
+    def teleport(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
--- evolving.py
+++ evolving.py
@@ -1,7 +1,53 @@
-#!/bin/env python3
+#!/bin/env python2
+
+import pygame, random, sys, math
+from pygame.locals import *
+
+from animal import Animal
 
 def main():
-    print("Hello world!")
+    # init phase
+    size = 600
+    pygame.init()
+    s = pygame.display.set_mode((size, size))
+    clock = pygame.time.Clock()
+    s.fill((255,255,255))
+
+    nb_animals = 30
+
+    genomes = ["+5A00i-1A000iMx", "+5A00i-1A000iMx-5A00iMy", "+5A00i-1A000iMx-Ai5M00y"]
+
+    animals = [ Animal(random.choice(genomes)) for _ in range(nb_animals) ]
+    for a in animals:
+        a.teleport(
+            random.randrange(0, 600, 1),
+            random.randrange(0, 600, 1),
+            random.uniform(0, 6.28)
+        )
+
+    while True:
+        deltatime = float(clock.tick(50)) / 1000
+        s.fill((255, 255, 255))
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sys.exit(0)
+
+        for a in animals:
+            # two last arguments are:
+            # - max speed in pixels per second
+            # - max angular speed in radian per second
+            a.update({}, deltatime, 50, 5)
+            if a.x > size: a.x -= size
+            if a.x < 0: a.x += size
+            if a.y > size: a.y -= size
+            if a.y < 0: a.y += size
+            # draw a body
+            pygame.draw.circle(s, 0, (int(a.x), int(a.y)), 8)
+            # draw a head
+            pygame.draw.circle(s, 0, (int(a.x + math.cos(a.theta)*7), int(a.y + math.sin(a.theta)*7)), 5)
+
+        pygame.display.update()
+
 
 if __name__ == '__main__':
     main()
