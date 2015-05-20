89895bb59cbdaba4850d3b68c8a806e14d3ed38e
MaximWolpher <maximwolpher@gmail.com>
2015-05-20 21:11:13 +0200

    Sim change

--- Sim.py
+++ Sim.py
@@ -1,19 +1,26 @@
 import pygame, random, sys
 from pygame.locals import *
 
-# start point
-xs = 300
-ys = 300
 
 size = 600
-
 pygame.init()
 s=pygame.display.set_mode((size, size))
 s.fill((255,255,255))
 
+nr = 5
 
-img = pygame.Surface((20, 20))
-dirs = 0
+img = pygame.Surface((20,20))
+xs = nr*[None]
+ys = nr*[None]
+dirs = nr*[None]
+for i in range(nr):
+    dirs[i] = random.randint(0,3)
+    xs[i] = random.randint(0,size)
+    ys[i] = random.randint(0,size)
+
+print(dirs)
+print(xs)
+print(ys)
 clock = pygame.time.Clock()
 
 def turn(cur):
@@ -30,25 +37,32 @@ def turn(cur):
         elif turn < 25 and cur == 3:nex = 0
         elif turn > 75 and cur == 3:nex = 2
     return nex
-    
+
     
 
 while True:
     clock.tick(10)
-
+    s.fill((255, 255, 255))
     for e in pygame.event.get():
         if e.type == QUIT:
             sys.exit(0)
-    dirs = turn(dirs)
-    if xs < 0:xs = size
-    if xs > size:xs = 0
-    if ys < 0:ys = size
-    if ys > size:ys = 0
-    if dirs==0:ys += 20;
-    elif dirs==1:xs += 20
-    elif dirs==2:ys -= 20
-    elif dirs==3:xs -= 20
 
-	
-    s.fill((255, 255, 255))	
-    s.blit(img, (xs, ys));pygame.display.update()
+    for i in range(0,nr):
+        if xs[i] < 0:xs[i] = size
+        if xs[i] > size:xs[i] = 0
+        if ys[i] < 0:ys[i] = size
+        if ys[i] > size:ys[i] = 0
+
+        dirs[i] = turn(dirs[i])
+        
+        if dirs[i]==0:ys[i] += 20;
+        elif dirs[i]==1:xs[i] += 20
+        elif dirs[i]==2:ys[i] -= 20
+        elif dirs[i]==3:xs[i] -= 20
+
+        
+        s.blit(img, (xs[i], ys[i]))
+
+        
+        
+        pygame.display.update()
