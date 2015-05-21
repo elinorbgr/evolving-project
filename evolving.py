ad562161a41d5455dacf4fed01d29851841a84ab
Victor Berger <victor.berger@m4x.org>
2015-05-21 11:05:38 +0200

    Refacto simulator to be graphics independant.

--- evolving.py
+++ evolving.py
@@ -3,27 +3,20 @@
 import pygame, random, sys, math
 from pygame.locals import *
 
-from animal import Animal
+from simulator import Simulator
 
 def main():
     # init phase
-    size = 600
+    (width, heigth) = (800, 600)
+
     pygame.init()
-    s = pygame.display.set_mode((size, size))
+    s = pygame.display.set_mode((width, heigth))
     clock = pygame.time.Clock()
     s.fill((255,255,255))
 
-    nb_animals = 30
-
     genomes = ["+5A00i-1A000iMx", "+5A00i-1A000iMx-5A00iMy", "+5A00i-1A000iMx-Ai5M00y"]
 
-    animals = [ Animal(random.choice(genomes)) for _ in range(nb_animals) ]
-    for a in animals:
-        a.teleport(
-            random.randrange(0, 600, 1),
-            random.randrange(0, 600, 1),
-            random.uniform(0, 6.28)
-        )
+    sim = Simulator(width, heigth, 30, genomes)
 
     while True:
         deltatime = float(clock.tick(50)) / 1000
@@ -32,15 +25,10 @@ def main():
             if e.type == QUIT:
                 sys.exit(0)
 
-        for a in animals:
-            # two last arguments are:
-            # - max speed in pixels per second
-            # - max angular speed in radian per second
-            a.update({}, deltatime, 50, 5)
-            if a.x > size: a.x -= size
-            if a.x < 0: a.x += size
-            if a.y > size: a.y -= size
-            if a.y < 0: a.y += size
+        sim.update(deltatime)
+
+        # display the animals
+        for a in sim.animals:
             # choose a color:
             col = pygame.Color(int((100-a.energy)*255/100), int(a.energy*255/100), 0, 255)
             # draw a body
---
+++ simulator/__init__.py
@@ -0,0 +1,27 @@
+import random
+
+from animal import Animal
+
+class Simulator:
+    def __init__(self, space_width, space_height, animal_count, genomes):
+        self.width = space_width
+        self.height = space_height
+        self.genomes = genomes
+        self.animals = [ Animal(random.choice(genomes)) for _ in range(animal_count) ]
+        for a in self.animals:
+            a.teleport(
+                random.randrange(0, self.width, 1),
+                random.randrange(0, self.height, 1),
+                random.uniform(0, 6.28)
+            )
+
+    def update(self, timestep):
+        for a in self.animals:
+            # two last arguments are:
+            # - max speed in pixels per second
+            # - max angular speed in radian per second
+            a.update({}, timestep, 50, 5)
+            if a.x > self.width: a.x -= self.width
+            if a.x < 0: a.x += self.width
+            if a.y > self.height: a.y -= self.height
+            if a.y < 0: a.y += self.height
