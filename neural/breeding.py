c1d65ff1be6a35fb555e76fe3e541f014cdb31a8
MaximWolpher <maximwolpher@gmail.com>
2015-06-01 22:42:03 +0200

    Breeding fixed

--- neural/breeding.py
+++ neural/breeding.py
@@ -3,16 +3,68 @@
 import random
 
 
-def breed(genA,genB):
-    aSize = int(len(genA)/2)
-    bSize = int(len(genB)/2)
+def breed_cs(genA,genB):
+    aCut = random.randint(0,len(genA))
+    bCut = random.randint(0,len(genB))
+    childA = []
+    childB = []
+    for i in range(0,aCut):
+        childA.append(genA[i])
+    for i in range(0,bCut):
+        childB.append(genB[i])
+    for i in range(bCut,len(genB)):
+        childA.append(genB[i])
+    for i in range(aCut,len(genA)):
+        childB.append(genA[i])
+    
+    return [''.join(childA),''.join(childB)]
 
-    aMix = ''.join(random.sample(genA,len(genA)))
-    bMix = ''.join(random.sample(genB,len(genB)))
 
-    child1 = ''.join(random.sample(aMix[0:aSize]+bMix[0:bSize],aSize+bSize))
-    child2 = ''.join(random.sample(aMix[aSize:]+bMix[bSize:],aSize+bSize))
-    return [child1,child2]
+
+def breed_uni(genA,genB):
+    childA = []
+    childB = []
+    size = min(len(genA),len(genB))
+    for i in range(size):
+        r = random.randint(0,100)
+        if r < 50:
+            childA.append(genA[i])
+            childB.append(genB[i])
+        else:
+            childA.append(genB[i])
+            childB.append(genA[i])
+    if len(genA)< len(genB):
+        for i in range(size,len(genB)):
+            childB.append(genB[i])
+    else:
+        for i in range(size,len(genA)):
+            childA.append(genA[i])
+    return [''.join(childA),''.join(childB)]
+
+def breed_2px(genA,genB):
+    childA = []
+    childB = []
+    r = []
+    size = min(len(genA),len(genB))
+    for i in range(2):
+        r.append(random.randint(0,size))
+    
+    for i in range(0,min(r)):
+        childA.append(genA[i])
+        childB.append(genB[i])
+    for i in range(min(r),max(r)):
+        childA.append(genB[i])
+        childB.append(genA[i])
+    for i in range(max(r),size):
+        childA.append(genA[i])
+        childB.append(genB[i])
+    if len(genA)< len(genB):
+        for i in range(size,len(genB)):
+            childB.append(genB[i])
+    if len(genA)> len(genB):
+        for i in range(size,len(genA)):
+            childA.append(genA[i])
+    return [''.join(childA),''.join(childB)]
 
 def mutate(gen):
     ind = []
